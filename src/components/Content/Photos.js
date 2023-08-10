b71d26c43b80fda01f29d0ddf1f4cbb098ecc98a
Molly <dudumikah@gmail.com>
2023-08-10 16:27:11 +0500

    Что-то сделал

--- src/components/Content/Photos.js
+++ src/components/Content/Photos.js
@@ -4,21 +4,21 @@ import { useSelector } from 'react-redux'
 import './Styles/Photos.css'
 
 export default function Photos () {
-    // const photos = useSelector(state => state.photos);
+    const photos = useSelector(state => state.photos);
 
-    // console.log(`State ${photos}`);
-    // console.log(photos)
+    console.log(`State ${photos}`);
+    console.log(photos)
     
     return (
         <div className='photos-container'>
             Photos
-            {/* {photos.map((photo, index) => {
+            {photos.map((photo, index) => {
                 <div key={index}>
                     <img className='photo-box' src={URL.createObjectURL(photo)} alt={photo.name} />
                 </div>
             })}
             {console.log(photos.length)}
-            Photos */}
+            Photos
         </div>
     )
 }
