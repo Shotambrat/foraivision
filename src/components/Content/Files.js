5ebab4dc9450cd944da3afd8e39a1391fe21fd02
Molly <dudumikah@gmail.com>
2023-08-11 11:52:09 +0500

    ЧО то сделал10

--- src/components/Content/Files.js
+++ src/components/Content/Files.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import {store} from '../../store';
 
 export default function Files() {
+    const response = store.getState().token;
+    console.log(`TOKEN ISSS ${response.token}`);
+    console.log(response.token)
+
     return (
         <div>Files</div>
     )
