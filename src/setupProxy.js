18708c402767d02177332e90dca72cd9081b4760
Molly <dudumikah@gmail.com>
2023-08-11 12:06:48 +0500

    ЧО то сделал13

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
                 '^/api': '',
             },
             headers: {
-                'Authorization': 'y0_AgAAAABpirqGAApNHAAAAADpp4jEU3pFf3lWRSubGhaP8rq7UcHEb2c'
+                'Authorization': 'Bearer y0_AgAAAABpirqGAApNHAAAAADpp4jEU3pFf3lWRSubGhaP8rq7UcHEb2c'
             },
         })
     );
